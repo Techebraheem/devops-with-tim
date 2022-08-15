77aca1148da267a9249e96aeab74da1d78ffc19c
rasheedat jamiu <hartinuke16@gmail.com>
2022-08-15 11:49:33 +0100

    berlin

---
+++ Berlin/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__, static_url_path='', static_folder='')
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+PORT = int(os.environ.get("PORT", 8080))
+if __name__ == '__main__':
+    app.run(threaded=True,host='0.0.0.0',port=PORT)
